6903c999ba0ef0826874a03457b10e65d1b37664
Nikolay Pomogaev <klagefurt@gmail.com>
2024-06-02 22:52:49 +0300

    Add project files.

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace FlatDesign_WPF.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
