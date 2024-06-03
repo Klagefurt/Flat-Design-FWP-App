5b99d259a3fab4bf956cf912a0da75597443a6d4
Nikolay Pomogaev <klagefurt@gmail.com>
2024-06-03 18:45:05 +0300

    Created the backend part

---
+++ Core/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace FlatDesign_WPF.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> excute, Func<object, bool> canExecute = null)
+        {
+            _execute = excute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove {  CommandManager.RequerySuggested -= value;}
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
